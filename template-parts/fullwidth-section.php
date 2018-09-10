eb53e8acc51462be05176c3c3ebb4f9cee0697c3
Ash - SIlverless <ash@silverless.co.uk>
2018-09-10 10:13:13 +0100

    Tidy HTML

--- template-parts/fullwidth-section.php
+++ template-parts/fullwidth-section.php
@@ -1,62 +1,61 @@
-	</div></div><!--close row and container --> 
-	</div><!--section-->
-	
-<div id="<?php the_sub_field('section_id');?>" class="block <?php the_sub_field('effect');?>">
-	
-	
-	
-	<div class="sl-fullwidth image-holder" style="background-image: url(<?php the_sub_field('image'); ?>);"></div>
-	<div class="icon-wrapper">
-	
-	<div class="floating-icon">
-	<img src="<?php the_sub_field('icon');?>"/>
-	</div>
-	
-	<a href="#<?php the_sub_field('continue_reading_target');?>"><span></span></a>
-	
-	</div>
+</div></div><!--close row and container --> 
+</div><!--section-->
 
-
-	<div class="section">
-<div class="container"><!--reopen row and container -->
-<div class="row">
-		<h2 class="<?php the_sub_field('alignment');?> fade-heading"><?php the_sub_field('heading');?></h2>	
+	<div id="<?php the_sub_field('section_id');?>" class="block <?php the_sub_field('effect');?>">
+	
+			<div class="sl-fullwidth image-holder" style="background-image: url(<?php the_sub_field('image'); ?>);"></div>
 			
-		
-		
-		<div class="content align-<?php the_sub_field('alignment');?> <?php the_sub_field('drop_cap');?>"> 	
-		<?php if( have_rows('content') ):?>
-		<?php while ( have_rows('content') ) : the_row(); ?>
-		
-			
-				<?php the_sub_field('text');?>
-				
-					<?php endwhile;?>
-				
-									<p class="cta-read">
-					<a href="<?php the_sub_field('button_target');?>" class="cta"><?php the_sub_field('button_text');?><span></span></a>	
-				</p>
-		
-	<?php
-		else :
-		endif;
-		?>			
-</div>
-
-	    				<div class="next-section">		
+				<div class="container">
+					<div class="icon-wrapper">
 					
-				</div>	
-	</div></div><!--close row and container --> 
-	</div><!--section-->
-	
+						<div class="floating-icon">
+							<img src="<?php the_sub_field('icon');?>"/>
+						</div>
+						
+						<a href="#<?php the_sub_field('continue_reading_target');?>"><span></span></a>
+				
+					</div>
+				</div>
+		
+			<div class="section">
+				<div class="container"><!--reopen row and container -->
+				<div class="row">
+					<h2 class="<?php the_sub_field('alignment');?> fade-heading"><?php the_sub_field('heading');?></h2>	
+					
+						<div class="content align-<?php the_sub_field('alignment');?> <?php the_sub_field('drop_cap');?>"> 	
+							<?php if( have_rows('content') ):?>
+							<?php while ( have_rows('content') ) : the_row(); ?>
+							
+							
+							<?php the_sub_field('text');?>
+							
+							<?php endwhile;?>
+						
+							<p class="cta-read">
+							
+							<a href="<?php the_sub_field('button_target');?>" class="cta"><?php the_sub_field('button_text');?>
+								<span></span>
+							</a>
+								
+							</p>
+						
+							<?php
+							else :
+							endif;
+							?>			
+						</div>
+					
+					<div class="next-section"></div>
+						
+				</div></div><!--close row and container --> 
+			</div><!--section-->
 
+		<div class='clearfix'></div>	
 	
-	    <div class='clearfix'></div>	
-	   	
-</div><!--block-->
+	</div><!--block-->
 
 <div class="container"><!--reopen row and container -->
 <div class="row">	
-	
-	
-	
+
+
+
