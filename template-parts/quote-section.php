42d02890b9485777c89bbe14b96ca9f7869793d1
Ash - SIlverless <ash@silverless.co.uk>
2018-09-17 17:00:14 +0100

    Add quote section

---
+++ template-parts/quote-section.php
@@ -0,0 +1,21 @@
+
+</div>
+</div><!--close row and container -->
+
+
+<section id="quote" class="testimonial auto-height" style="background-image: url(<?php the_sub_field('image'); ?>);">
+
+	
+	<div class="content-wrapper">
+	
+		<p><?php the_sub_field('quote');?></p>
+		
+		<p><?php the_sub_field('quoted_by');?></p>
+	
+	</div><!--end content wrapper-->
+
+</section>
+
+<div class="container"><!--reopen row and container -->
+<div class="row">
+	
